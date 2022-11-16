0bc7fab8f15e2b9a89cd39cede7f4558d7f5b478
Reedtztoja <patryk.patryg@gmail.com>
2022-11-16 10:57:24 +0100

    2

---
+++ class/user.class.php
@@ -0,0 +1,31 @@
+<?php
+class User {
+    private $db;
+    private int $id;
+    private string $login;
+    private string $passwordHash;
+    private string $firstName;
+    private string $lastName;
+
+    public function __construct(string $login, string $password) 
+    {
+        $this->login = $login;
+        $this->password_hash = password_hash($password, PASSWORD_ARGON2I);
+        global $db;
+        $this->
+    }
+    
+    public function isAuth() : bool {
+
+    }
+    public function login() {
+        global $db;
+    }
+    public function logout() {
+        global $db;
+    }
+    public function register() {
+        global $db;
+    }
+}
+?>
